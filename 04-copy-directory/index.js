b56d2010c9483bbaf64d9b2936f39b666cda91a1
thefoku <leekengham@gmail.com>
2025-01-16 16:26:53 +0400

    fix: add feature to update files if they were removed or modified

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,41 @@
-// const fs = require('fs');
 const path = require('node:path');
 const fsPromises = require('node:fs/promises');
 
 const sourcePath = path.resolve(__dirname, 'files');
 const targetPath = path.resolve(__dirname, 'files-copy');
 const files = fsPromises.readdir(sourcePath, { withFileTypes: true });
-
-// console.log(targetPath);
+const targetFiles = fsPromises.readdir(targetPath, { withFileTypes: true });
 
 fsPromises.mkdir(targetPath, { recursive: true });
 
-files
-  .then((files) => {
-    files.forEach((file) => {
-      if (file.isFile()) {
-        const src = path.resolve(sourcePath, file.name);
+function clearTargetDir(targetFiles) {
+  targetFiles.then((files) => {
+    if (files.length > 0) {
+      files.forEach((file) => {
         const target = path.resolve(targetPath, file.name);
-        console.log(src);
-        fsPromises.copyFile(src, target);
-      }
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+        fsPromises.unlink(target);
+      });
+    }
   });
+}
+
+function addFiles(files) {
+  files
+    .then((files) => {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          const src = path.resolve(sourcePath, file.name);
+          const target = path.resolve(targetPath, file.name);
+          fsPromises.copyFile(src, target);
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+clearTargetDir(targetFiles);
+setTimeout(() => {
+  addFiles(files);
+}, 1000);
