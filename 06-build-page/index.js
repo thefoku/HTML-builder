ddbdc127e27301095bc5d6daabe0b8cea0ddfd38
thefoku <leekengham@gmail.com>
2025-01-21 01:02:52 +0400

    fix: replace setTimeout functions with async in task 6

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,123 +5,115 @@ const fsPromises = require('node:fs/promises');
 const stylesDir = path.resolve(__dirname, 'styles');
 const projectDist = path.resolve(__dirname, 'project-dist');
 const componentsDir = path.resolve(__dirname, 'components');
-
-const components = fsPromises.readdir(componentsDir, { withFileTypes: true });
-const styles = fsPromises.readdir(stylesDir, { withFileTypes: true });
-
-fsPromises.mkdir(projectDist, { recursive: true });
-
-const styleWrite = fs.createWriteStream(path.resolve(projectDist, 'style.css'));
-
-const templateFilePath = path.resolve(__dirname, 'template.html');
-const templateWriter = fs.createWriteStream(
-  path.resolve(projectDist, 'index.html'),
-);
-
-const arr = [];
-
-fs.readFile(templateFilePath, 'utf8', (error, templateData) => {
-  if (error) throw error;
-  let templateFileData = templateData;
-
-  components.then((comps) => {
-    comps.forEach((file) => {
-      fs.readFile(
-        path.resolve(componentsDir, file.name),
-        'utf8',
-        (error, compData) => {
-          if (error) throw error;
-          const fileName = path.basename(file.name).split('.')[0];
-          templateFileData = templateFileData.replace(
-            `{{${fileName}}}`,
-            compData.slice(0, -1),
-          );
-          arr.push(templateFileData);
-          if (arr[comps.length - 1])
-            templateWriter.write(arr[comps.length - 1]);
-        },
-      );
-    });
-  });
-});
-
-// bundle all styles
-styles
-  .then((dirs) => {
-    dirs.forEach((file) => {
-      if (file.isFile() && path.extname(file.name) === '.css') {
-        const fileToRead = path.resolve(stylesDir, file.name);
-        fs.readFile(fileToRead, 'utf8', (error, data) => {
-          if (error) throw error;
-          styleWrite.write(data);
-        });
-      }
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-// Move assets to project-dist
 const assetsDir = path.resolve(__dirname, 'assets');
-const assets = fsPromises.readdir(assetsDir, { withFileTypes: true });
-
 const projectAssets = path.resolve(projectDist, 'assets');
-fsPromises.mkdir(projectAssets, { recursive: true });
 
-function copyAssets(assets) {
-  assets
-    .then((dirs) => {
-      dirs.forEach((dir) => {
-        const assetDirectoryFiles = fsPromises.readdir(
-          path.resolve(assetsDir, dir.name),
-          {
-            withFileTypes: true,
-          },
-        );
+async function buildPage() {
+  // Create project-dist folder
+  await fsPromises.mkdir(projectDist, { recursive: true });
 
-        // create folders
-        fsPromises.mkdir(path.resolve(projectAssets, dir.name), {
-          recursive: true,
-        });
+  // Replace HTML-template
+  await processTemplate();
 
-        assetDirectoryFiles.then((files) => {
-          files.forEach((file) => {
-            const src = path.resolve(assetsDir, dir.name, file.name);
-            const target = path.resolve(projectAssets, dir.name, file.name);
-            fsPromises.copyFile(src, target);
-          });
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // Building CSS
+  await bundleStyles();
+
+  // Copying assets
+  await resetAssets();
+  await copyAssets();
 }
-setTimeout(() => {
-  copyAssets(assets);
-}, 300);
 
-setTimeout(() => {
-  const projectAssetsDirs = fsPromises.readdir(projectAssets, {
+// Replace HTML-template
+async function processTemplate() {
+  const templateFilePath = path.resolve(__dirname, 'template.html');
+  const templateData = await fsPromises.readFile(templateFilePath, 'utf8');
+  const components = await fsPromises.readdir(componentsDir, {
     withFileTypes: true,
   });
-  deleteOldAssets(projectAssetsDirs);
-}, 100);
 
-function deleteOldAssets(assets) {
-  assets
-    .then((dirs) => {
-      dirs.forEach((dir) => {
-        if (dirs.length > 0) {
-          fsPromises.rm(path.resolve(projectAssets, dir.name), {
-            recursive: true,
-            force: true,
-          });
-        }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  let resultHTML = templateData;
+
+  for (const file of components) {
+    if (file.isFile() && path.extname(file.name) === '.html') {
+      const componentName = path.basename(file.name, '.html');
+      const componentContent = await fsPromises.readFile(
+        path.resolve(componentsDir, file.name),
+        'utf8',
+      );
+      resultHTML = resultHTML.replace(
+        `{{${componentName}}}`,
+        componentContent.slice(0, -1),
+      );
+    }
+  }
+
+  const outputPath = path.resolve(projectDist, 'index.html');
+  await fsPromises.writeFile(outputPath, resultHTML);
 }
+
+// Bundle CSS
+async function bundleStyles() {
+  const styles = await fsPromises.readdir(stylesDir, { withFileTypes: true });
+  const styleWrite = fs.createWriteStream(
+    path.resolve(projectDist, 'style.css'),
+  );
+
+  for (const file of styles) {
+    if (file.isFile() && path.extname(file.name) === '.css') {
+      const fileContent = await fsPromises.readFile(
+        path.resolve(stylesDir, file.name),
+        'utf8',
+      );
+      styleWrite.write(fileContent);
+    }
+  }
+}
+
+async function resetAssets() {
+  try {
+    await fsPromises.access(projectAssets);
+
+    await fsPromises.rm(projectAssets, { recursive: true, force: true });
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      console.error(`Error removing old assets: ${err.message}`);
+    }
+  }
+}
+
+// Copying assets
+async function copyAssets() {
+  await fsPromises.mkdir(projectAssets, { recursive: true });
+  const assets = await fsPromises.readdir(assetsDir, { withFileTypes: true });
+
+  for (const asset of assets) {
+    const src = path.resolve(assetsDir, asset.name);
+    const dest = path.resolve(projectAssets, asset.name);
+
+    if (asset.isDirectory()) {
+      await copyDirectory(src, dest);
+    } else {
+      await fsPromises.copyFile(src, dest);
+    }
+  }
+}
+
+// Copying folder content
+async function copyDirectory(src, dest) {
+  await fsPromises.mkdir(dest, { recursive: true });
+  const files = await fsPromises.readdir(src, { withFileTypes: true });
+
+  for (const file of files) {
+    const srcPath = path.resolve(src, file.name);
+    const destPath = path.resolve(dest, file.name);
+
+    if (file.isDirectory()) {
+      await copyDirectory(srcPath, destPath);
+    } else {
+      await fsPromises.copyFile(srcPath, destPath);
+    }
+  }
+}
+
+// Build a page
+buildPage();
