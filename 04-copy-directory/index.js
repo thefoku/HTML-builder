796094ef90d19ba8e6c30b46515a2bbdd7357551
thefoku <leekengham@gmail.com>
2025-01-21 00:44:08 +0400

    fix: rewrite task 4 without SetTimeout function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,44 +3,43 @@ const fsPromises = require('node:fs/promises');
 
 const sourcePath = path.resolve(__dirname, 'files');
 const targetPath = path.resolve(__dirname, 'files-copy');
-const files = fsPromises.readdir(sourcePath, { withFileTypes: true });
 
-fsPromises.mkdir(targetPath, { recursive: true });
+async function clearTargetDir(targetFiles) {
+  if (!targetFiles) {
+    return;
+  }
 
-function clearTargetDir(targetFiles) {
-  if (targetFiles) {
-    targetFiles.then((files) => {
-      files.forEach((file) => {
-        fsPromises.rm(path.resolve(targetPath, file.name), {
-          recursive: true,
-          force: true,
-        });
-      });
+  for (const file of targetFiles) {
+    await fsPromises.rm(path.resolve(targetPath, file.name), {
+      recursive: true,
+      force: true,
     });
   }
 }
 
-function addFiles(files) {
-  files
-    .then((files) => {
-      files.forEach((file) => {
-        if (file.isFile()) {
-          const src = path.resolve(sourcePath, file.name);
-          const target = path.resolve(targetPath, file.name);
-          fsPromises.copyFile(src, target);
-        }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+async function addFiles(files) {
+  for (const file of files) {
+    if (!file.isFile()) {
+      return;
+    }
+    const src = path.resolve(sourcePath, file.name);
+    const target = path.resolve(targetPath, file.name);
+
+    await fsPromises.copyFile(src, target);
+  }
 }
 
-setTimeout(() => {
-  const targetFiles = fsPromises.readdir(targetPath, { withFileTypes: true });
-  clearTargetDir(targetFiles);
-}, 100);
+async function main() {
+  const files = await fsPromises.readdir(sourcePath, { withFileTypes: true });
 
-setTimeout(() => {
-  addFiles(files);
-}, 200);
+  await fsPromises.mkdir(targetPath, { recursive: true });
+
+  const targetFiles = await fsPromises.readdir(targetPath, {
+    withFileTypes: true,
+  });
+
+  await clearTargetDir(targetFiles);
+  await addFiles(files);
+}
+
+main();
